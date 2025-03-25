f07074110d37495174794a550d4ce99f6c51f4bb
Fonti Kar <f.kar@unsw.edu.au>
2025-03-26 08:22:38 +1100

    More docs to pass RCMD

--- R/helper.R
+++ R/helper.R
@@ -1,41 +1,41 @@
 #' Format relational database for display
 #'
-#' @param database 
+#' @param database traits.build object
 
 format_database_for_display <- function(database){
   flatten_database(database) |> 
     select(-ends_with(".x"),
            -ends_with("_id"),
            -starts_with("source"),
-           -c(methods,
-              description, 
-              assistants, 
-              dataset_curators, 
-              aligned_name,
-              binomial, 
-              trinomial, 
-              taxon_name_alternatives, 
-              sampling_strategy),
-           dataset_id, 
-           source_primary_citation) |> 
-    rename(genus = genus.y,
-           family = family.y,
-           taxon_rank = taxon_rank.y,
-           establishment_means = establishment_means.y) |> 
-    relocate(dataset_id, .before = "taxon_name") |> 
-    relocate(source_primary_citation, .after = "method_context_properties")
+           -c("methods",
+              "description", 
+              "assistants", 
+              "dataset_curators", 
+              "aligned_name",
+              "binomial", 
+              "trinomial", 
+              "taxon_name_alternatives", 
+              "sampling_strategy"),
+           "dataset_id", 
+           "source_primary_citation") |> 
+    rename(genus = "genus.y",
+           family = "family.y",
+           taxon_rank = "taxon_rank.y",
+           establishment_means = "establishment_means.y") |> 
+    relocate("dataset_id", .before = "taxon_name") |> 
+    relocate("source_primary_citation", .after = "method_context_properties")
 }
 
 
 #' Format database for download handler
 #'
-#' @param database 
+#' @param database traits.build object
 
 format_database_for_download <- function(database){
   flatten_database(database) |>
     select(-ends_with(".x")) |> 
-    rename(genus = genus.y,
-           family = family.y,
-           taxon_rank = taxon_rank.y,
-           establishment_means = establishment_means.y) 
+    rename(genus = "genus.y",
+           family = "family.y",
+           taxon_rank = "taxon_rank.y",
+           establishment_means = "establishment_means.y") 
 }
